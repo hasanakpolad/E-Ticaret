8d900ce66c9088d43675895d1cbcbb00cc9764bd
hasan.akpolad <hasanakpolad@gmail.com>
2021-11-26 17:38:14 +0300

    dev

--- Script/E-Ticaret-Sepet.js
+++ Script/E-Ticaret-Sepet.js
@@ -2,12 +2,38 @@ var product = document.getElementById('products');
 var user = window.localStorage.getItem('user')
 function GetBasket() {
     if (user == '') {
-        GetBasket()
+        GetBasket().then(res => function (params) {
+            if (res.rows.length != 0) {
+                product.removeAttribute('display')
+            }
+        }).catch()
     }
     else{
         GetProducts()
     }
 }
+function Temizle() {
 
-//Services
-//Create new Product Table If Not 
+}
+
+function SatınAl(params) {
+    if (user == '') {
+        alert('Lütfen devam etmek için giriş yapınız.')
+        window.open('/E-Ticaret/E-Ticaret-SignIn.html','_self')
+    }
+    else{
+        Temizle()
+        alert('Satın Alma işlemi başarılı.')
+    }
+}
+
+var user = window.localStorage.getItem('user')
+var ul = document.getElementById('ulList')
+if (user != null) {
+    ul.innerHTML = '<li class="nav-item"><a class="nav-link" href="E-Ticaret-SignIn.html" onclick="LogOut()"><i class="fas fa-sign-out-alt"></i> Çıkış Yap</a></li>'
+
+}
+
+function LogOut() {
+    return window.localStorage.clear()
+}
